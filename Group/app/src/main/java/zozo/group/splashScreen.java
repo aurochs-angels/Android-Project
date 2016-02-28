1f2a6e6a7bccfe248c6c5c70ddb93ee1d126bb9b
A. Nummikallio <aino.nummikallio@gmail.com>
2016-02-28 13:21:54 +0200

    Added files via upload

---
+++ Group/app/src/main/java/zozo/group/splashScreen.java
@@ -0,0 +1,23 @@
+package zozo.group;
+
+/**
+ * Created by nummikallio on 28.2.2016.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class splashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
+
